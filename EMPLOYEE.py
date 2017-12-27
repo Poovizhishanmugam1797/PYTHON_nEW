373f7701e44b28f7a1b75fd83943db59a1f7ffe5
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-27 23:28:11 +0530

    Create EMPLOYEE.py

---
+++ EMPLOYEE.py
@@ -0,0 +1,8 @@
+hrs=int(input("No Of Hrs               : "))
+rate=int(input("Rate Per Hour          : "))
+weeks=int(input("No Of Weeks In Month  : "))
+pay=hrs*rate*weeks
+print(" Monthly Pay          : ",pay)
+print(" No Of Hrs            : ",hrs)
+print(" Rate Per Hour        : ",rate)
+print(" No Of Weeks In Month : ",weeks)
