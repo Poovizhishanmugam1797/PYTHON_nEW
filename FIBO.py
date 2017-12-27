97e55c6f6826c72000b2d988570ac0a85ef75854
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-27 23:29:47 +0530

    Create FIBO.py

---
+++ FIBO.py
@@ -0,0 +1,23 @@
+n1=int(input("Enter the size : "))
+n2=0
+n3=1
+lt=[]
+lt2=[]
+lt.append(n2)
+lt.append(n3)
+for i in range(0,(n1-2)):
+     n4=n2+n3
+     
+     lt.append(n4)
+     n2=n3
+     n3=n4
+print(lt)    
+     
+    
+     
+     
+     
+     
+     
+     
+
