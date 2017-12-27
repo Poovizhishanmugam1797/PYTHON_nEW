c7e0d50955c035ba9f150907d07fa417ff660c50
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-27 23:30:34 +0530

    Create SORT.py

---
+++ SORT.py
@@ -0,0 +1,29 @@
+n1=int(input("Enter the size : "))
+lt2=[]
+lt3=[]
+lt5=[]
+for i in range(0,n1):
+     lt=input()
+     lt2.append(lt)
+lt5=sorted(lt2)
+s4=" ".join(lt5)
+
+print(s4)
+s5=s4[::-1]
+print(s5)
+
+
+     
+
+     
+     
+    
+     
+    
+     
+     
+     
+     
+     
+     
+
