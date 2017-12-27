ad6364b9cb8a00b24e47cac73ab93ebf74d3fc27
Poovizhishanmugam1797 <34641878+Poovizhishanmugam1797@users.noreply.github.com>
2017-12-27 23:32:08 +0530

    Create MERGE.py

---
+++ MERGE.py
@@ -0,0 +1,26 @@
+s=input("Enter the String ")
+s1=input("Enter the String ")
+s3=s+s1
+count=0
+lt=[]
+s4="".join(s3)
+print(s4)
+
+for i in range(0,len(s4)):
+     if(s4[i].isupper()):
+          count=count+1
+          lt.append(s4[i])
+s5="".join(lt)
+print(s5)
+          
+
+          
+
+
+         
+              
+  
+          
+              
+         
+          
